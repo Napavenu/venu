bc916579935d91daafe43b0dfdf3f9d89b533026
Napavenu <99411557+Napavenu@users.noreply.github.com>
2022-02-20 07:44:04 +0530

    Add files via upload (#1)

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 alert('WELCOME TO MY WEBSITE');
 
-let name='Venu';
-console.log(name);
+let person={
+   name:'venu',
+   age:15
+};
 
+let firstname="Napa";
+let selectedcolour=['red','green'];
+selectedcolour[2]='black';
+let isApproved=false;
+function greet(name,lastname){
+    console.log('Hello'+ name+' '+lastname);
+}
+greet('Jeswanth' ,'vadlamudi');
 
-
-
-
-
-
+function square(number){
+    return number*number;
+}
+let number=square(15);
+console.log(number);
+    
 
